369e33b351da9b04a422ad441a1d84ed96c2860e
Лазарь <lazarus@MobileTerminal.abyss>
2021-02-03 01:49:18 +0300

    Add const in Command.java

--- chat_kvazi_28012021/commands/src/main/java/commands/Command.java
+++ chat_kvazi_28012021/commands/src/main/java/commands/Command.java
@@ -5,5 +5,5 @@ public class Command {
     public static final String AUTH = "/auth";
     public static final String AUTH_OK = "/authok";
     public static final String PRIVATE_MSG = "/w";
-
+    public static final String REG_NEW_USR = "/new";
 }
