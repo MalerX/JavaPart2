ffe2729e7cbf03eb11b5e7fffefd77bf01beb596
MalerX <malleerx@gmail.com>
2021-03-01 17:09:09 +0300

    Refactoring Stage.java

--- ragerace/Stage.java
+++ ragerace/Stage.java
@@ -4,8 +4,5 @@ public abstract class Stage {
     protected int length;
     protected String description;
 
-    public String getDescription() {
-        return description;
-    }
     public abstract void go(Car car);
 }
